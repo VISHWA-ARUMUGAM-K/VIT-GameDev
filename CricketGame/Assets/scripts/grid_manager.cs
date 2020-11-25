ec88e17ba8831c89bb0cac553c8890299d867b70
VISHWA-ARUMUGAM-K <vishwakrishoooo@gmail.com>
2020-11-25 16:06:17 +0530

    first commit
    made a grid 3x2

---
+++ CricketGame/Assets/scripts/grid_manager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class grid_manager : MonoBehaviour
+{
+    private int rows = 3;
+    private int cols = 2;
+    private float tilesize = 1;
+    void Start()
+    {
+        gengrid();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void gengrid()
+    {
+        GameObject referenceTile = (GameObject)Instantiate(Resources.Load("stadium-ground-texture"));
+        for(int row=0; row < rows; row++)
+        {
+            for(int col = 0; col < cols; col++)
+            {
+
+                GameObject tile = (GameObject)Instantiate(referenceTile, transform);
+
+                float posX = col * tilesize;
+                float posY = row * -tilesize;
+
+                tile.transform.position = new Vector3(posX, posY);
+            }
+        }
+        Destroy(referenceTile);
+    }
+}
---
+++ CricketGame/Assets/scripts/player.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField]
+    private float _speed = 6;
+    void Start()
+    {
+        transform.position = new Vector3(0, 0, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        calmove();
+    }
+    void calmove()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+
+        float vertical = Input.GetAxis("Vertical");
+
+        Vector3 direction = new Vector3(horizontal, vertical, 0);
+
+        transform.Translate(direction * _speed * Time.deltaTime);
+
+
+
+    }
+}
