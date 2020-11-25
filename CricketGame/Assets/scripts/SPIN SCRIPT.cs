4b1a9f459e36f4a2fbfd6be494d0de127d7a0155
VISHWA-ARUMUGAM-K <vishwakrishoooo@gmail.com>
2020-11-25 17:51:25 +0530

    commit 3

---
+++ CricketGame/Assets/scripts/SPIN SCRIPT.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SPINSCRIPT : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public bool acti;
+    public GameObject myobj;
+    public Text Textfield;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()    
+    {
+        if (acti == true)
+        {
+            myobj.SetActive(true);
+        }
+        else
+        {
+            myobj.SetActive(false);
+        }
+
+
+    }
+    public void SetText(string text)
+    {
+        Textfield.text = text;
+    }
+}
