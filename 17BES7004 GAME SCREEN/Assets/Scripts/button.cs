8a822108c2047e068bcf48d6846ec931ea79bcfa
VISHWA-ARUMUGAM-K <vishwakrishoooo@gmail.com>
2020-11-26 16:25:57 +0530

    summit 3
    level 2 partially completed

---
+++ 17BES7004 GAME SCREEN/Assets/Scripts/button.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class button : MonoBehaviour
+{
+    [SerializeField]
+    private Text text1;
+    public void SetText(string text)
+    {
+        text1.text = text;
+    }
+
+}
